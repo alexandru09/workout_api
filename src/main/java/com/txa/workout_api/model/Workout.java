c3f3f5236f6588b650eec796b91e95e3e8dddd99
alexandru09 <tudor.alexandru1996@gmail.com>
2020-04-22 20:29:01 +0300

    Explicit collection name

--- src/main/java/com/txa/workout_api/model/Workout.java
+++ src/main/java/com/txa/workout_api/model/Workout.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "workouts")
 public class Workout {
 
     @Id
