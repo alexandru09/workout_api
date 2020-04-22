ccadd170761224613076dee50fa4a537538b811f
alexandru09 <tudor.alexandru1996@gmail.com>
2020-04-22 20:25:23 +0300

    Modified Repository

--- src/main/java/com/txa/workout_api/Repository/WorkoutRepository.java
+++ src/main/java/com/txa/workout_api/Repository/WorkoutRepository.java
@@ -1,12 +1,11 @@
 package com.txa.workout_api.Repository;
 
-import java.util.List;
-
 import com.txa.workout_api.model.Workout;
 import com.txa.workout_api.model.WorkoutSplit;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface WorkoutRepository extends MongoRepository<Workout, String> {
-    public List<Workout> findBySplitType(WorkoutSplit splitType);
+    public Workout findBySplitType(WorkoutSplit splitType);
+    public void deleteById(String id);
 }
